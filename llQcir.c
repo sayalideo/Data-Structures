69de70f9df9222731a4dc6c24dc276622e262853
Saiy <sayalideo137@gmail.com>
2019-03-21 20:46:24 +0530

    Queue using Circular Linked List

--- llQcir.c
+++ llQcir.c
@@ -1,4 +1,8 @@
 //7.2 Queue using Circular Linked List
+//Only rear is used
+//rear->next is front
+//Enqueues at rear->next and rear becomes newly added
+//Dequeues at rear->next i.e. front
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
@@ -25,38 +29,46 @@ void enqueue(Q *q,int ele)
 	else
 	{
         p->next = q->rear->next;
-		q->rear->next=p;
+        q->rear->next=p;
+        q->rear=p;
 	}
 }
 
 void display(Q q)
 {
+    Node *temp;
 	if(q.rear==NULL)
 	{
 		printf("Stack Underflow\n");
 	}
 	else
 	{
-		while(q.rear!=NULL)
+        temp=q.rear->next;
+        printf("Elements in Queue : ");
+		do
 		{
-			printf("%d\n",q.front->data);
-			q.front=q.front->next;
-		}	
+			printf("%d ",temp->data);
+			temp=temp->next;
+		}while(temp!=q.rear->next);	
+        printf("\n");
 	}	
 }
 
 void dequeue(Q *q)
 {
-	if(q->front==NULL)
+	if(q->rear==NULL)
 	{
 		printf("Stack Underflow\n");
 	}
+    else if(q->rear->next == q->rear)    //When single node is deleted
+    {
+        printf("%d Popped",q->rear->data);
+        q->rear=NULL;
+    }    
 	else
 	{
-		printf("%d Popped",q->front->data);
-		q->front=q->front->next;
-        if(q->front == NULL)
-            q->rear=NULL;
+		printf("%d Popped",q->rear->next->data);
+		q->rear->next=q->rear->next->next;
 	}
 }
 
@@ -67,7 +79,7 @@ int main()
     int op,ele;
     do
     {
-        printf("Enter Choice 1:Enqueue Element\n2.Dequeue Element\n3.Display\n5.Exit :");
+        printf("1:Enqueue Element\n2.Dequeue Element\n3.Display\n4.Exit\nEnter Choice :");
         scanf("%d",&op);
         switch(op)
         {
@@ -82,7 +94,7 @@ int main()
             case 3:
                     display(q);
                     break;
-            case 5:
+            case 4:
                     printf("Thank You!");
                     exit(0);
             default:
@@ -92,3 +104,69 @@ int main()
     }while(1);
     return 0;
 }
+/*
+sayali@sayali:~/Repositories/Data-Structures$ cc llQcir.c
+sayali@sayali:~/Repositories/Data-Structures$ ./a.out
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :1
+Enter Element : 12
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :1
+Enter Element : 13
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :1
+Enter Element : 14
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :3
+Elements in Queue : 12 13 14 
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :2
+12 Popped1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :2
+13 Popped1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :2
+14 Popped1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :2
+Stack Underflow
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :3
+Stack Underflow
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :6
+Wrong Input -_- ! Please Enter the Correct Option
+1:Enqueue Element
+2.Dequeue Element
+3.Display
+4.Exit
+Enter Choice :4
+Thank You!sayali@sayali:~/Repositories/Data-Structures$ */
